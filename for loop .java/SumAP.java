dfec0b1717aaf769abf85f43fca50d4f23b38db5
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:22:49 +0530

    Update SumAP.java

--- for loop .java/SumAP.java
+++ for loop .java/SumAP.java
@@ -9,4 +9,19 @@ current += d;
 }
 System.out.println("Sum: " + sum);
 }
+
+}
+
+
+public class SumAP {
+public static void main(String[] args) {
+int a = 1, d = 3, n = 5;
+int sum = 0;
+int current = a;
+for(int i = 1; i <= n; i++) {
+sum += current;
+current += d;
+}
+System.out.println("Sum: " + sum);
+}
 }
