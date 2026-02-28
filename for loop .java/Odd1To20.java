5b89da531d5cb5a298515106dd07d91d5f61fb5b
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:20:15 +0530

    Update Odd1To20.java

--- for loop .java/Odd1To20.java
+++ for loop .java/Odd1To20.java
@@ -8,3 +8,15 @@ public class Odd1To20 {
     }
     
 }
+
+
+public class Odd1To20 {
+    public static void main(String[] args) {
+        for(int i=1;i<=20;i++){
+            if(i%2!=0){
+                System.out.println(i+" ");
+            }
+        }
+    }
+    
+}
