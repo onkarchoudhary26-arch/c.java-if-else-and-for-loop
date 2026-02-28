7dd96a084605e1eeb5c34bb6600567dc7d552fc0
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:19:08 +0530

    Update HarmonicSeries.java

--- for loop .java/HarmonicSeries.java
+++ for loop .java/HarmonicSeries.java
@@ -6,4 +6,16 @@ for(int i = 1; i <= n; i++) {
 sum += 1.0 / i;
 }System.out.println("Sum: " + sum);
 }
+
+}
+
+
+public class HarmonicSeries {
+public static void main(String[] args) {
+int n = 5;
+double sum = 0.0;
+for(int i = 1; i <= n; i++) {
+sum += 1.0 / i;
+}System.out.println("Sum: " + sum);
+}
 }
