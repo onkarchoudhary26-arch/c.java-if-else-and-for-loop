ae92de29d15f72c911767f3948a3f475abf7e4a3
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:23:32 +0530

    Update SumOfSquares.java

--- for loop .java/SumOfSquares.java
+++ for loop .java/SumOfSquares.java
@@ -8,3 +8,15 @@ sum += i * i;
 System.out.println("Sum: " + sum);
 }
 }
+
+
+public class SumOfSquares {
+public static void main(String[] args) {
+int n = 5;
+int sum = 0;
+for(int i = 1; i <= n; i++) {
+sum += i * i;
+}
+System.out.println("Sum: " + sum);
+}
+}
