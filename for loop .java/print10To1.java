7463723b997264174a4e20aa05d7c751b4eb7e40
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:24:31 +0530

    Update print10To1.java

--- for loop .java/print10To1.java
+++ for loop .java/print10To1.java
@@ -6,3 +6,13 @@ public class print10To1 {
     }
     
 }
+
+
+public class print10To1 {
+    public static void main(String[] args) {
+        for(int i=10;i>=1;i--){
+            System.out.println(i+" ");
+        }
+    }
+    
+}
