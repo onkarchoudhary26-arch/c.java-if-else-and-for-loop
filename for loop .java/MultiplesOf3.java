55fc5f3b7f591298557ca7c3ea23302c559ac3a7
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:20:02 +0530

    Update MultiplesOf3.java

--- for loop .java/MultiplesOf3.java
+++ for loop .java/MultiplesOf3.java
@@ -6,3 +6,13 @@ public class MultiplesOf3 {
     }
     
 }
+
+
+public class MultiplesOf3 {
+    public static void main(String[] args) {
+        for(int i=0;i<=30;i+=3){
+            System.out.println(i+" ");
+        }
+    }
+    
+}
