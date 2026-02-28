634b96ca6e3b01ef25c85fb07e3e93019906aa19
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:19:28 +0530

    Update LCM.java

--- for loop .java/LCM.java
+++ for loop .java/LCM.java
@@ -9,4 +9,19 @@ break;
 }
 }
 }
+
+}
+
+
+public class LCM {
+public static void main(String[] args) {
+int n1 = 4, n2 = 6;
+int max = Math.max(n1, n2);
+for(;; max++) {
+if(max % n1 == 0 && max % n2 == 0) {
+System.out.println("LCM: " + max);
+break;
+}
+}
+}
 }
