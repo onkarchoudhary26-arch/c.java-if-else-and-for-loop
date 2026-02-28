cd67103d387c0d1ed8d6cffd6b41d3d9033faa45
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:17:21 +0530

    Update CountDigits.java

--- for loop .java/CountDigits.java
+++ for loop .java/CountDigits.java
@@ -7,4 +7,17 @@ count++;
 }
 System.out.println("Digits: " + count);
 }
+
+}
+
+public class CountDigits {
+public static void main(String[] args) {
+int n = 5829;
+int count = 0;
+for(; n != 0; n /= 10) {
+count++;
+}
+System.out.println("Digits: " + count);
+}
+
 }
