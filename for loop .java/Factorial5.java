d23684987be3bb1eb21c695c411361782df481de
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:17:54 +0530

    Update Factorial5.java

--- for loop .java/Factorial5.java
+++ for loop .java/Factorial5.java
@@ -7,4 +7,17 @@ fact *= i;
 }
 System.out.println("Factorial: " + fact);
 }
+
+}
+
+
+public class Factorial5 {
+public static void main(String[] args) {
+int fact =1;
+int n =5;
+for(int i = 1; i <= n; i++) {
+fact *= i;
+}
+System.out.println("Factorial: " + fact);
+}
 }
