83f2ffcf3778658d61dcb095b844cd9820954ff4
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:17:37 +0530

    Update Even1To20.java

--- for loop .java/Even1To20.java
+++ for loop .java/Even1To20.java
@@ -8,3 +8,16 @@ public class Even1To20 {
     }
     
 }
+
+
+
+public class Even1To20 {
+    public static void main(String[] args) {
+        for(int i=2;i<=20;i++){
+            if(i%2==0){
+                System.out.println(i+" ");
+            }
+        }
+    }
+    
+}
