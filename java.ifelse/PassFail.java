c0347e72aa987866355c431e8b371e82db8be677
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:32:14 +0530

    Update PassFail.java

--- java.ifelse/PassFail.java
+++ java.ifelse/PassFail.java
@@ -9,4 +9,19 @@ System.out.println("Pass");
 else
 System.out.println("Fail");
 }
+
+}
+
+
+import java.util.Scanner;
+public class PassFail {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter marks: ");
+int marks = sc.nextInt();
+if(marks >= 40)
+System.out.println("Pass");
+else
+System.out.println("Fail");
+}
 }
