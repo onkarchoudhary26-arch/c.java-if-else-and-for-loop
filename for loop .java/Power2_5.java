339c666221f499b842886285173caeb1b0057a96
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:21:00 +0530

    Update Power2_5.java

--- for loop .java/Power2_5.java
+++ for loop .java/Power2_5.java
@@ -7,4 +7,17 @@ result *= base;
 }
 System.out.println("Result: " + result);
 }
+
+}
+
+
+public class Power2_5 {
+public static void main(String[] args) {
+int base = 2, exp = 5;
+int result = 1;
+for(int i = 1; i <= exp; i++) {
+result *= base;
+}
+System.out.println("Result: " + result);
+}
 }
