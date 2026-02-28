0d88a39f16ff309e62dbd8834e81a51329e108e7
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:30:21 +0530

    Update GreatestTwo.java

--- java.ifelse/GreatestTwo.java
+++ java.ifelse/GreatestTwo.java
@@ -11,4 +11,21 @@ System.out.println("Greatest: " + a);
 else
 System.out.println("Greatest: " + b);
 }
+
+}
+
+
+import java.util.Scanner;
+public class GreatestTwo {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter first number: ");
+int a = sc.nextInt();
+System.out.print("Enter second number: ");
+int b = sc.nextInt();
+if(a > b)
+System.out.println("Greatest: " + a);
+else
+System.out.println("Greatest: " + b);
+}
 }
