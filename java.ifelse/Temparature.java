d2ea3dbdb625d65427cbf86df1a6c47421ee91d8
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:33:56 +0530

    Update Temparature.java

--- java.ifelse/Temparature.java
+++ java.ifelse/Temparature.java
@@ -9,4 +9,19 @@ System.out.println("Hot");
 else
 System.out.println("Cool");
 }
+
+}
+
+
+import java.util.Scanner;
+public class Temparature {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter temperature: ");
+int temp = sc.nextInt();
+if(temp > 25)
+System.out.println("Hot");
+else
+System.out.println("Cool");
+}
 }
