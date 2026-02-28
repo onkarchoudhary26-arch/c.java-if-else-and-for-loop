ee39cd88cba8ca8d1811e8518e019a785633e590
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:22:30 +0530

    Update Sum1To100.java

--- for loop .java/Sum1To100.java
+++ for loop .java/Sum1To100.java
@@ -6,4 +6,16 @@ sum += i;
 }
 System.out.println("Sum: " + sum);
 }
+
+}
+
+
+public class Sum1To100 {
+public static void main(String[] args) {
+int sum = 0;
+for(int i = 1; i <= 100; i++) {
+sum += i;
+}
+System.out.println("Sum: " + sum);
+}
 }
