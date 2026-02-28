2eac6556100d78976eb0c1ddb0a392a1e9a1c0a0
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:31:56 +0530

    Update NumberCheck.java

--- java.ifelse/NumberCheck.java
+++ java.ifelse/NumberCheck.java
@@ -11,4 +11,21 @@ System.out.println("Negative");
 else
 System.out.println("Zero");
 }
+
+}
+
+
+import java.util.Scanner;
+public class NumberCheck {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter number: ");
+int n = sc.nextInt();
+if(n > 0)
+System.out.println("Positive");
+else if(n < 0)
+System.out.println("Negative");
+else
+System.out.println("Zero");
+}
 }
