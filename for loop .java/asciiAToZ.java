f614dae737f8ce1e54d70def66be2263ae7f2a84
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:24:17 +0530

    Update asciiAToZ.java

--- for loop .java/asciiAToZ.java
+++ for loop .java/asciiAToZ.java
@@ -6,3 +6,13 @@ public class asciiAToZ {
     }
     
 }
+
+
+public class asciiAToZ {
+    public static void main(String[] args) {
+        for(char c='A';c<='Z';c++){
+            System.out.println(c +" "+ (int)c);
+        }
+    }
+    
+}
