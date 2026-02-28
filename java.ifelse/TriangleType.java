16c78ee5e2ddb8c8332fb37ae355ea3a49e4c3e8
onkarchoudhary26-arch <onkarchoudhary26@gmail.com>
2026-02-28 21:34:20 +0530

    Update TriangleType.java

--- java.ifelse/TriangleType.java
+++ java.ifelse/TriangleType.java
@@ -18,4 +18,28 @@ public class TriangleType {
             System.out.println("Scalene");
         }
     }
+
+}
+
+
+import java.util.Scanner;
+
+public class TriangleType {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Enter side 1: ");
+        int a = sc1.nextInt();
+        System.out.print("Enter side 2: ");
+        int b = sc1.nextInt();
+        System.out.print("Enter side 3: ");
+        int c = sc1.nextInt();
+        
+        if (a == b && b == c) {
+            System.out.println("Equilateral");
+        } else if (a == b || b == c || a == c) {
+            System.out.println("Isosceles");
+        } else {
+            System.out.println("Scalene");
+        }
+    }
 }
